7a2ced643e1641a606c6eaf8801a132f7023117b
suttirakcha <suttirak.char@gmail.com>
2025-05-19 09:04:13 +0700

    style: update CSS of the modal

--- src/components/ModalText.tsx
+++ src/components/ModalText.tsx
@@ -6,11 +6,11 @@ export const MainModalText = ({ children } : PropsWithChildren) => {
 }
 
 export const ModalText = ({ text, isOpen, isRounded } : ModalTextProps) => {
+  const visibleClassName = isOpen ? 'opacity-1 visible' : 'opacity-0 invisible';
+  const roundedClassName = isRounded ? "rounded-full" : "rounded-2xl";
   return (
-    <div 
-      className={`${isOpen ? 'opacity-1 visible' : 'opacity-0 invisible'} transition-all absolute h-full w-full top-0 left-0 bg-black/75 z-50 text-white flex items-center text-center justify-center`}
-    >
-      <div className={`${isRounded ? "rounded-full" : "rounded-2xl"} bg-pink-400 px-10 py-6 max-w-[90%]`}>
+    <div className={`${visibleClassName} modal`}>
+      <div className={`${roundedClassName} bg-pink-400 px-10 py-6 max-w-[90%]`}>
         {typeof text === "string" ? <MainModalText>{text}</MainModalText> : text}
       </div>
     </div>
